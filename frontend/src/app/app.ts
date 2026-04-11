73f216e92041f3fc5450bab0cb403c46af61a12d
Sanzhar <omarkhanovsanzh@gmail.com>
2026-04-12 01:39:33 +0500

    basic files edited

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,18 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `
-    <nav class="bg-blue-900 text-white px-6 py-4 flex items-center gap-3">
-      <span class="text-xl font-bold">🌍 AirWatch</span>
-      <span class="text-sm text-blue-300">Air Quality Monitor</span>
-    </nav>
-    <main class="min-h-screen bg-gray-100">
-      <router-outlet />
-    </main>
-  `,
+  imports: [RouterOutlet, NavbarComponent],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
 })
 export class App {}
